732a9595b4f3ec232abc0fe2edb648cfe5b0f1c2
shravankumargulvadi <shravangulvadi@gmail.com>
2024-10-19 18:48:35 +0000

    integrate catch2 framework and add an example

---
+++ examples/test/simple_test.cc
@@ -0,0 +1,6 @@
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Framework integration test", "[integration]") {
+    REQUIRE(1 + 1 == 2);
+    REQUIRE(std::string("Hello") + " World" == "Hello World");
+}
