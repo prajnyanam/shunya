6919b7806e8016b4d07f006ef98d1c08e6c87e92
shravankumargulvadi <shravangulvadi@gmail.com>
2025-03-02 01:49:49 +0000

    define individual states

---
+++ axillary-node-manager/includes/surveillance_mode_state.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "state.h"
+#include "axillary_node_manager.h" 
+
+namespace axillary_node_manager {
+
+class SurveillanceMode : public State {
+ public:
+  void onEnter(AxillaryNodeManager& manager) override;
+
+  void onUpdate(AxillaryNodeManager& manager) override;
+
+  void onEvent(AxillaryNodeManager& manager, Event event) override;
+};
+
+}  // namespace axillary_node_manager
+
+
---
+++ axillary-node-manager/includes/wait_for_command_state.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "state.h"
+#include "axillary_node_manager.h" 
+
+namespace axillary_node_manager {
+
+class WaitForCommand : public State {
+ public:
+  void onEnter(AxillaryNodeManager& manager) override;
+
+  void onUpdate(AxillaryNodeManager& manager) override;
+
+  void onEvent(AxillaryNodeManager& manager, Event event) override;
+};
+
+}  // namespace axillary_node_manager
+
