2b1afa272aa3073c420243ada5bcdf1d73819246
shravankumargulvadi <shravangulvadi@gmail.com>
2025-03-02 01:47:57 +0000

    define state base class and events enum

---
+++ axillary-node-manager/includes/state.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <memory>
+
+namespace axillary_node_manager {
+
+class AxillaryNodeManager;  // Forward declaration
+
+
+enum class Event {
+  eSensorSuiteReady,
+  eRunSurveillanceMode,
+  eRunChaseMode,
+  eMasterNodeCommLost,
+  eSensorFaulted,
+  eSensorSuiteCommLost,
+  eHibernationComplete
+};
+
+// Abstract base class for states.
+class State {
+ public:
+  virtual ~State() = default;
+
+  // Called once when the state is entered.
+  virtual void onEnter(AxillaryNodeManager& manager) {}
+
+  // Called on every update cycle."
+  virtual void onUpdate(AxillaryNodeManager& manager) {}
+
+  // Called when an event is triggered.
+  virtual void onEvent(AxillaryNodeManager& manager, Event event) {}
+
+  // Called right before transitioning out of this state.
+  virtual void onExit(AxillaryNodeManager& manager) {}
+};
+
+}  // namespace axillary_node_manager
+
