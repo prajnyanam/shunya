a11421551f4a6fca2dec585d0f20d499d3abee51
Srivatsa <gramas1996@gmail.com>
2025-10-18 19:41:20 -0700

    Docker changes to improve CI and attaching volumes

---
+++ tools/download_datasets.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Used to download datasets from Google Drive
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from google.oauth2 import service_account
+import io
+
+if __name__ == '__main__':
+    breakpoint()
